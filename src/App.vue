ca2c2ee81dd83abcd7a40514b270086678a912c2
Pongsatorn Nitithammawoot <biig_pongsatorn@hotmail.com>
2018-09-20 11:48:47 +0700

    add chat-vue-custom-element

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="side-bar">
+      <div class="side-bar-menu" @click="$router.push({ name: 'vue-custom-element' })">
+        vue-custom-element
+      </div>
+      <div class="side-bar-menu" @click="$router.push({ name: 'vue-web-component-wrapper' })">
+        vue-web-component-wrapper
+      </div>
+    </div>
+    <div class="wc-body">
+      <router-view/>
+    </div>
+    <div class="wc-plugin">
+      PLUGIN
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+</style>
+
+<style scoped>
+.side-bar {
+  float: left;
+  width: 200px;
+  border-right: 1px solid #ccc;
+  height: 100vh;
+}
+.side-bar-menu {
+  float: left;
+  width: 100%;
+  padding: 20px;
+}
+.side-bar-menu:hover {
+  color: blue;
+  cursor: pointer;
+}
+.wc-body {
+  float: left;
+  width: calc(100vw - 1px - 200px - 200px);
+}
+.wc-plugin {
+  float: left;
+  width: 200px;
 }
 </style>
---
+++ src/views/VueCustomElement.vue
@@ -0,0 +1,29 @@
+<template>
+  <chat-vue-custom-element :id="id" :name="name" :prefix="prefix" :thread.prop="thread" :msg.prop="msg" @sendMessage="sent"/>
+</template>
+
+<script>
+export default {
+  name: 'VueCustomElement',
+  data () {
+    return {
+      thread: {
+        img: 'https://vignette.wikia.nocookie.net/line/images/b/b7/1511.png/revision/latest?cb=20150809133444'
+      },
+      id: 99271,
+      prefix: 'Mr.',
+      name: 'Brown',
+      msg: [
+        { sender: 'user', message: 'Hello :)' },
+        { sender: 'user', message: 'How are you ?' }
+      ]
+    }
+  },
+  methods: {
+    sent (e) {
+      const value = e.detail[0]
+      console.log(value)
+    }
+  }
+}
+</script>
