29720dd75475e70d4efafb055af5f36bf859b61c
Pongsatorn Nitithammawoot <biig_pongsatorn@hotmail.com>
2018-09-24 12:03:04 +0700

    completely

--- src/views/VueCustomElement.vue
+++ src/views/VueCustomElement.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div class="left">
+    <div class="left" v-if="isReady">
       <chat-vue-custom-element :id="id" :name="name" :prefix="prefix" :thread.prop="thread" :msg.prop="msg" @sendMessage="sent"/>
+    </div>
+    <div class="left" v-else>
+      loading...
     </div>
     <div class="right">
       <chat-log :logs="logs"/>
@@ -11,11 +14,13 @@
 
 <script>
 import ChatLog from '@/components/ChatLog.vue'
+import SystemJS from 'systemjs/dist/system-production.js'
 
 export default {
   name: 'VueCustomElement',
   data () {
     return {
+      isReady: false,
       thread: {
         img: 'https://vignette.wikia.nocookie.net/line/images/b/b7/1511.png/revision/latest?cb=20150809133444'
       },
@@ -29,6 +34,10 @@ export default {
       logs: []
     }
   },
+  async created () {
+    await SystemJS.import('../script/chat-vue-custom-element.js')
+    this.isReady = true
+  },
   methods: {
     sent (e) {
       this.logs.push(e.detail[0])
@@ -40,7 +49,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .left {
   float: left;
   width: calc(100% - 1px - 200px);
--- src/views/VueWebComponentWrapper.vue
+++ src/views/VueWebComponentWrapper.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div class="left">
+    <div class="left" v-if="isReady">
       <chat-vue-web-component-wrapper :id="id" :name="name" :prefix="prefix" @sendMessage="sent"/>
+    </div>
+    <div class="left" v-else>
+      loading...
     </div>
     <div class="right">
       <chat-log :logs="logs"/>
@@ -11,16 +14,23 @@
 
 <script>
 import ChatLog from '@/components/ChatLog.vue'
+import SystemJS from 'systemjs/dist/system-production.js'
 
 export default {
   name: 'VueWebComponentWrapper',
   data () {
     return {
+      isReady: false,
       id: 99271,
       prefix: 'Mr.',
       name: 'Brown (vue-web-component-wrapper)',
       logs: []
     }
+  },
+  async mounted () {
+    // await SystemJS.import('https://unpkg.com/vue@2.5.17/dist/vue.js')
+    await SystemJS.import('../script/chat-vue-web-component-wrapper.js')
+    this.isReady = true
   },
   methods: {
     sent (e) {
@@ -32,3 +42,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.left {
+  float: left;
+  width: calc(100% - 1px - 200px);
+}
+.right {
+  float: left;
+  width: 200px;
+  border-left: 1px solid #ccc;
+  height: 100vh;
+}
+</style>
